3c523327bd11e9e2c5509b5d140b2dadf030ad04
cas3zn <allancheboiw0@gmail.com>
2023-02-01 18:20:35 +0800

    Complete Repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+    
+    let char = '';
+    for (i = 0; i < num; i++){
+        char += string;
+    }
 
+    return char;
 };
 
 // Do not edit below this line
