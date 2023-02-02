753ae9f965ae5f0d993ac780457900dfee7ad1de
cas3zn <allancheboiw0@gmail.com>
2023-02-03 03:53:50 +0800

    Solve calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+// Solved
+
+// Take 2 arguments and return their sum
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+// Solved
+
+// Take 2 arguments and return their subtraction
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+// Solved
+
+// Take an array and add all the numbers in the array
+const sum = function(array) {
+	return array.reduce((callbackFunction, currentValue) => callbackFunction + currentValue, 0);
 };
 
-const multiply = function() {
+// Solved
 
+// Takes an array and gives the multiplication of all the values
+const multiply = function(array) {
+  return array.length ? array.reduce((callbackFunction, currentValue) => callbackFunction * currentValue): 0;
 };
 
-const power = function() {
-	
+// Solved
+
+// Take 2 arguments and raise one to the power of the other
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+// Solved
+
+// Takes an argument and finds its factorial
+const factorial = function(arg) {
+	if (arg === 0) {
+    return 1;
+  }
+
+  let product = 1;
+  for (let i=arg;i>0;i--){
+    product *= i;
+  }
+
+  return product;
 };
 
 // Do not edit below this line
