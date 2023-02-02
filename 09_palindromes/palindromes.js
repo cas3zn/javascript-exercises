d13698c23348a5bf645e50df42ed73fd56c80f01
cas3zn <allancheboiw0@gmail.com>
2023-02-03 04:30:47 +0800

    Solve palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    return str.toLowerCase().replace(/[^a-z]/g, "").split('').reverse().join('') == str.toLowerCase().replace(/[^a-z]/g, "");
 };
 
 // Do not edit below this line
