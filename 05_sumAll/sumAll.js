ec6169225056466ea436cbbd1fe65f0950393816
cas3zn <allancheboiw0@gmail.com>
2023-02-03 01:30:31 +0800

    solution to sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    // If null return zero
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+    if (0 > min || 0 > max) return 'ERROR';
+    let sum = 0;
 
+    // Find the minimum between the two values
+    if (min > max){
+        const temp = min;
+        min = max;
+        max = temp;
+    } 
+
+    for(let count = min; count < max + 1; count++){
+        sum += count;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
